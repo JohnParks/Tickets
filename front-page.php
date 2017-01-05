98c12c2ff3a043b26e5d571e41c77b0210aea13c
JohnParks <john.parks@brafton.com>
2017-01-05 14:42:02 -0500

    Completed left side of homepage slider, working on adding in right show listings

--- front-page.php
+++ front-page.php
@@ -6,6 +6,100 @@
 
 						<div id="home-page-slider">
 
+							<?php 
+
+							$slider_query = get_home_slider();
+
+							$slides; // array that will hold all "slides" (which are arrays containing show title, images, link et al)
+
+							// open a loop for $slider_query to build out the slider HTML
+							if ( $slider_query->have_posts() ) : while ( $slider_query->have_posts() ) : $slider_query->the_post();
+
+								$slide['title'] = $post->post_title;
+								$slide['link'] = get_permalink();
+								$slide['poster'] = get_the_post_thumbnail( $post->ID, "small" );
+
+								//using "slider_image" meta field, grab attachment URL and store here
+								$background_ID = get_post_meta( $post->ID, "slider_image", true );
+								$backgroundURL = wp_get_attachment_url( $background_ID );
+								$slide['background'] = $backgroundURL;
+
+								// lastly, add this slide to our slides array
+								$slides[] = $slide;
+
+							endwhile;endif;
+
+							wp_reset_postdata();
+
+							/*echo "<pre>";
+							print_r( $slides );
+							echo "</pre>";*/
+
+							// begin a foreach block to build out the actual URL, iterating through our slides
+							?>
+
+							<div id='gallery-block'>
+
+							<?php
+								foreach ( $slides as $slide ) {
+							?>
+								<div class="slide" style="background-image: url('<?php echo $slide["background"]; ?>');" >
+									<div class='poster'><?php echo $slide['poster']; ?></div>
+									<div class='show-info'>
+										<h3><?php echo $slide['title']; ?></h3>
+										<a href="<?php echo $slide['link']; ?>" class="filled-buy" ><div>Buy Tickets</div></a>
+										<div class="more-info"><a href="<?php echo $slide['link']; ?>" >More Info</a> ></div>
+									</div>
+								</div>
+							<?php	} ?>
+
+							</div>
+
+							<div id="slider-listing">
+								<?php
+								// let's build out the listing on the right side of the slider
+								foreach ( $slides as $slide ) {
+									?>
+									<div class="show-listing">
+										<div class='poster'><?php echo $slide['poster']; ?></div>
+										<div class='show-info'>
+											<div class="show-title"><?php echo $slide['title']; ?></div>
+											<a href="<?php echo $slide['link']; ?>" class="empty-buy" ><div>Buy Tickets</div></a>
+										</div>
+									</div>
+									<?php
+								}
+
+								?>
+							</div>
+
+							<script>
+							var slides = $("#gallery-block").children();
+
+							var someTime;
+
+							function toggleThings( cntr ) {
+								$($("#gallery-block > div")[cntr]).toggle();
+								cntr = cntr == 3 ? 0 : cntr+1;
+								$($("#gallery-block > div")[cntr]).toggle();
+
+								someTime = setTimeout( toggleThings, 4000, cntr );
+							}
+
+							toggleThings( 0 );
+
+							/*$("#gallery-block > div:gt(0)").hide();
+
+							setInterval(function() { 
+							  $('#gallery-block > div:first')
+							    .fadeOut(1000)
+							    .next()
+							    .fadeIn(1000)
+							    .end()
+							    .appendTo('#gallery-block');
+							},  5000);*/
+							</script>
+
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							
